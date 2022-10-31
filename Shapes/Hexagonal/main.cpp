85c2723d9e4e242da4c7c3b90c7c915bd53f36e8
3bdullahkama1 <abdallah10kamal@gmail.com>
2022-10-31 07:55:12 +0200

    Hexagonal Shape

---
+++ Shapes/Hexagonal/main.cpp
@@ -0,0 +1,35 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(800, 600), "Circle...");
+
+    CircleShape circle(100);
+    circle.setFillColor(Color(150, 20, 50));
+    circle.setPosition(Vector2f(200, 150));
+
+    // 3 points for triangle shape...
+    circle.setPointCount(6);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+
+    return 0;
+}
